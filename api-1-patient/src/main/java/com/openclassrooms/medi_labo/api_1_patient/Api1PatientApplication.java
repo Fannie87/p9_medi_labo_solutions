7504e0d1f6b8a3cc7764b6e581475ddafc53cd21
Fannie <fannie.reyt@gmail.com>
2024-08-06 19:06:15 +0200

    Commit initial

---
+++ api-1-patient/src/main/java/com/openclassrooms/medi_labo/api_1_patient/Api1PatientApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.medi_labo.api_1_patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api1PatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api1PatientApplication.class, args);
+	}
+
+}
---
+++ api-1-patient/src/test/java/com/openclassrooms/medi_labo/api_1_patient/Api1PatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.medi_labo.api_1_patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api1PatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
